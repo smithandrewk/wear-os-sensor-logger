1e0074e979826deea4fac47037a0b63cc00e33b6
Andrew <andrewsmith1025@gmail.com>
2023-07-05 10:29:19 -0400

    register and write gyro and linear accel

--- sensor-logger/app/src/main/java/com/delta/sensor_logger/MainActivity.kt
+++ sensor-logger/app/src/main/java/com/delta/sensor_logger/MainActivity.kt
@@ -15,9 +15,13 @@
  */
 package com.delta.sensor_logger
 
+import android.content.Intent
+import android.content.IntentFilter
 import android.hardware.Sensor
 import android.hardware.SensorManager
+import android.os.BatteryManager
 import android.os.Bundle
+import android.os.SystemClock
 import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -35,6 +39,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.lifecycleScope
 import androidx.wear.compose.material.MaterialTheme
 import androidx.wear.compose.material.Text
 import com.delta.sensor_logger.presentation.theme.WearAppTheme
@@ -43,33 +48,79 @@ import java.io.FileOutputStream
 import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import org.json.JSONObject
 
 
 class MainActivity : ComponentActivity() {
     private lateinit var mSensorManager: SensorManager
     private lateinit var mAccelerometerListener: AccelerometerListener
+    private lateinit var mLinearAccelerationListener: LinearAccelerationListener
+    private lateinit var mGyroscopeListener: GyroscopeListener
     private lateinit var externalFilesDir : File
-    private val appStartTimeReadable = SimpleDateFormat("yyyy-MM-dd_HH_mm_ss", Locale.ENGLISH).format(Date())
-    private lateinit var f: FileOutputStream
-    private val mainViewModel = MainViewModel()
+    private lateinit var appStartTimeReadable: String
+    private lateinit var appStartTimeNanos: String
+    private lateinit var fAccel: FileOutputStream
+    private lateinit var fLinear: FileOutputStream
+    private lateinit var fGyro: FileOutputStream
+    private lateinit var fLog: FileOutputStream
+    private lateinit var mainViewModel: MainViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+
+        appStartTimeNanos = "${SystemClock.elapsedRealtimeNanos()}"
+        appStartTimeReadable = SimpleDateFormat("yyyy-MM-dd_HH_mm_ss", Locale.ENGLISH).format(Date())
+
         externalFilesDir = getExternalFilesDir(null)!!
-        val f = FileOutputStream(File(externalFilesDir,"${appStartTimeReadable}.csv"))
+        File(externalFilesDir, appStartTimeReadable).mkdir()
+        fAccel = FileOutputStream(File(externalFilesDir,"$appStartTimeReadable/acceleration.csv"))
+        fLinear = FileOutputStream(File(externalFilesDir,"$appStartTimeReadable/linear_acceleration.csv"))
+        fGyro = FileOutputStream(File(externalFilesDir,"$appStartTimeReadable/gyroscope.csv"))
+        fLog = FileOutputStream(File(externalFilesDir,"$appStartTimeReadable/log.csv"))
+
+        try {
+            val json = JSONObject()
+                .put("SystemClock.elapsedRealtimeNanos", appStartTimeNanos)
+                .put("App Start Time Readable", appStartTimeReadable)
+            File(externalFilesDir, "$appStartTimeReadable/metadata.json").appendText(json.toString())
+        } catch (e: Exception) { e.printStackTrace() }
+
+        mainViewModel = MainViewModel()
         mSensorManager = getSystemService(SENSOR_SERVICE) as SensorManager
-        mAccelerometerListener = AccelerometerListener(f,mainViewModel)
+        mAccelerometerListener = AccelerometerListener(fAccel,mainViewModel)
+        mLinearAccelerationListener = LinearAccelerationListener(fLinear,mainViewModel)
+        mGyroscopeListener = GyroscopeListener(fGyro,mainViewModel)
         mSensorManager.registerListener(mAccelerometerListener,mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),SensorManager.SENSOR_DELAY_NORMAL)
+        mSensorManager.registerListener(mGyroscopeListener,mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE),SensorManager.SENSOR_DELAY_NORMAL)
+        mSensorManager.registerListener(mLinearAccelerationListener,mSensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION),SensorManager.SENSOR_DELAY_NORMAL)
+
+        lifecycleScope.launch {
+            while(true) {
+                val batteryStatus: Intent? = registerReceiver(null,
+                    IntentFilter(Intent.ACTION_BATTERY_CHANGED)
+                )
+                val batteryPct: Float? = batteryStatus?.let { intent ->
+                    val level: Int = intent.getIntExtra(BatteryManager.EXTRA_LEVEL, -1)
+                    val scale: Int = intent.getIntExtra(BatteryManager.EXTRA_SCALE, -1)
+                    level * 100 / scale.toFloat()
+                }
+                fLog.write("${SystemClock.elapsedRealtimeNanos()},battery,$batteryPct\n".toByteArray())
+                delay(300000)
+            }
+        }
 
         setContent {
             WearApp(mainViewModel)
         }
     }
-
     override fun onDestroy() {
         super.onDestroy()
         mSensorManager.unregisterListener(mAccelerometerListener)
-        this.f.close()
+        this.fAccel.close()
+        this.fGyro.close()
+        this.fLinear.close()
     }
 }
 
